0c16a6cedd92623fb79621be310ac09bb96f32d3
bearjam <sam.croce@gmail.com>
2020-08-13 23:05:25 -0400

    git ignore

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express"),
-  bodyParser = require("body-parser"),
-  exphbs = require("express-handlebars"),
-  logger = require("morgan");
+    bodyParser = require("body-parser"),
+    exphbs = require("express-handlebars"),
+    logger = require("morgan");
 
 var PORT = process.env.PORT || 3000;
 
@@ -15,14 +15,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Make public a static folder
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Handlebars
 app.set("views", "./views");
 app.engine(
-  "handlebars",
-  exphbs({
-    defaultLayout: "main"
-  })
+    "handlebars",
+    exphbs({
+        defaultLayout: "main"
+    })
 );
 app.set("view engine", "handlebars");
 
@@ -30,10 +31,10 @@ app.set("view engine", "handlebars");
 require("./routes/apiRoutes")(app);
 
 // Start the server
-app.listen(PORT, function() {
-  console.log(
-    "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-    PORT,
-    PORT
-  );
+app.listen(PORT, function () {
+    console.log(
+        "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+        PORT,
+        PORT
+    );
 });
