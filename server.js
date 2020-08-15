19796ccc88a1dd9ccaf39786a317328ed959144c
bearjam <sam.croce@gmail.com>
2020-08-14 21:11:20 -0400

    broke the app

--- server.js
+++ server.js
@@ -1,40 +1,40 @@
-var express = require("express"),
-    bodyParser = require("body-parser"),
-    exphbs = require("express-handlebars"),
-    logger = require("morgan");
+const express = require("express");
+const bodyParser = require("body-parser"); //JSON responses
+const mongoose = require("mongoose"); //Mongo object modelling 
+const request = require("request"); //Makes http calls
+const cheerio = require("cheerio"); //Scraper
 
-var PORT = process.env.PORT || 3000;
+// Require all models
+const db = require("./models");
+
+// Port configuration for local/Heroku
+const PORT = process.env.PORT || process.argv[2] || 8080;
 
 // Initialize Express
-var app = express();
+const app = express();
 
-// Use morgan logger for logging requests
-app.use(logger("dev"));
-// Parse request body as JSON
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-// Make public a static folder
-app.use(express.static("public"));
+// Use body-parser for handling form submissions
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Handlebars
-app.set("views", "./views");
-app.engine(
-    "handlebars",
-    exphbs({
-        defaultLayout: "main"
-    })
-);
+const exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Routes
-require("./routes/apiRoutes")(app);
+// Use express.static to serve the public folder as a static directory
+app.use(express.static("public"));
+// Controllers
+const router = require("./controllers/api.js");
+app.use(router);
+// Connect to the Mongo DB
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
 
 // Start the server
 app.listen(PORT, function () {
-    console.log(
-        "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-        PORT,
-        PORT
-    );
+    console.log(`This application is running on port: ${PORT}`);
 });
